2eb137583a9df1fe8037644cde28cb4a4aebd8c8
Amanda La <anla@buffalo.edu>
2020-09-23 22:56:18 -0400

    linked password feature to child->admin mode switch #31

--- axel_extension/popup.js
+++ axel_extension/popup.js
@@ -1,45 +1,3 @@
-// let changeColor = document.getElementById('changeColor');
-// let options = document.getElementById('options');
-
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-
-// //Grabs whether the user is a first time admin(just installed)
-// chrome.storage.sync.get('first_time', function(result){
-//   console.log('Value currently is '+ result.key);
-//   options.setAttribute('value', result.key);
-//   // if(1){
-//   //   chrome.browserAction.setPopup({popup: 'popup_sign_in.html'});
-//   // }
-// });
-
-// var k = 0;
-// //change color of site
-// changeColor.onclick = function(element) {
-//     let color = element.target.value;
-//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//       chrome.tabs.executeScript(
-//           tabs[0].id,
-//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//     });
-// }
-// //click options button, change front end of popup GUI based on past use
-// options.onclick = function(element){
-//   let test = element.target.value;
-//   // const align = options.getAttribute('value');
-//     if(test == 'true'){
-//       chrome.browserAction.setPopup({popup: 'popup_create_pass.html'});
-//     }
-//     else{
-//       chrome.browserAction.setPopup({popup: 'popup_sign_in.html'});
-//     }
-// }
-  
-
-
-
 // This documents purpose is for the use of javascript functionality.
 // It enables things such as switching between buttons and Tabs.
 //Furture implementations may also go inside of this file
@@ -50,75 +8,111 @@
 document.addEventListener('DOMContentLoaded', function(){
 	
 	
-  //		The next two lines make it so Admin Options does not appear 
-  //		Upon the intial load of the extension
+  //		The next six lines make it so "Admin Options", "sign_in", and
+  //    "create_pass" do not appear upon the intial load of the extension
     var childB = document.getElementById("Admin Mode");
+    var sign_in = document.getElementById("sign_in");
+    var create_pass = document.getElementById("create_pass");
     childB.style.display = "none";
-
-  //    Applied logic seen above to hide Password Prompt
-    var passB = document.getElementById("sign_in");
-    var passC = document.getElementById("create_pass");
-    passB.style.display = "none";
-    passC.style.display = "none";
+    sign_in.style.display = "none";
+    create_pass.style.display = "none";
   
+  /************************************************************************/
   //		The below section Allows for Switching to the Child Mode Tab.
   //		It Controls the buttons appearing when clicked along
   //		with the removal of unwanted buttons	
+  //    passB and passC are added within this event listener in the event that
+  //    a child/admin clicks the "Child Mode" button while within 
+  //    the create/enter password phase
+  /************************************************************************/
   var child = document.getElementById("default");
   child.addEventListener("click",function(){
     var adminB = document.getElementById("Admin Mode");
     adminB.style.display = "none";
     var childB = document.getElementById("Child Mode");
     childB.style.display = "block";
-    passB.style.display = "none";
+    sign_in.style.display = "none";
+    create_pass.style.display = "none";
   });
   
+  /********************************************************************** */
   //		The below section Allows for Switching to the Admin Mode Tab.
   //		It Controls the buttons appearing when clicked along
   //		with the removal of unwanted buttons
+  /********************************************************************** */
   var admin = document.getElementById("Admin");
+
+  /*    chrome.storage.sync.get()
+   *    Grabs the key 'first_time' from storage (initialized in background.js) 
+   *    and stores the object in the result parameter. 
+   *    Then, we set the value of admin to the value stored in result
+   *    (this is accessed by result.first_time).
+   *    Works with 'var admin' declared above this comment block.
+  */
 
   chrome.storage.sync.get('first_time', function(result){
     console.log('Value currently is '+ result.first_time);
     admin.setAttribute('value', result.first_time);
   });
 
+  /********************************************************************** */
+
+      /*    admin.addEventListener()
+       *    "element.target. value" grabs the value we stored in admin  
+       *    and store the value in the variable "firstTime".
+       *    From here, we check if our admin is a first time admin. 
+       *    If value == 'true', we ask them to create a password, hide all
+       *    <div> elements except "create_pass" in "popup.html", and change the value
+       *    of the key 'first_time' to 'false' in storage. 
+       *    Else, we ask them to enter the established password and hide all <div>
+       *    elements except "sign_in" in "popup.html". 
+       * 
+       *    In the near future, the if statement will be updated with a 
+       *    method to store the created password. The else statement will be
+       *    updated with a method to check if the entered password matches 
+       *    the password stored in our database.
+      */
+
+  /********************************************************************** */
+
+
   admin.addEventListener("click", function(element){
-    let test = element.target.value;
+    let firstTime = element.target.value;
     var childB = document.getElementById("Child Mode");
     var adminB = document .getElementById("Admin Mode");
-    var passB = document.getElementById("sign_in");
+    var sign_in = document.getElementById("sign_in");
     var create_pass = document.getElementById("create_pass");
 
-      //  if a new admin, create password. else, enter established password
-    if(test == 'true'){
+    if(firstTime == 'true'){
       childB.style.display = "none";
       adminB.style.display = "none";
       create_pass.style.display = "block";
-      passB.style.display = "none";
+      sign_in.style.display = "none";
       chrome.storage.sync.set({first_time: 'false'}, function(){
         console.log('Value is set to ' + false);
       });
-      element.target.value = 'false';
+      // element.target.value = 'false';
     }
     else{
       childB.style.display = "none";
       adminB.style.display = "none";
       create_pass.style.display = "none";
-      passB.style.display = "block";
+      sign_in.style.display = "block";
     }
   })
 
-  //    This portion of code added to admin event listener checks if
-  //    app is installed for the first time, and allows admin to create
-  //    password.
+  /********************************************************************** */
+  /*    If the submit button within the password phase is clicked,
+   *    take admin to admin page.
+   *    ***********(CURRENTLY NOT FUNCTIONAL)**************
+  */
+ /********************************************************************** */
   var submit = document.getElementById("submit");
   submit.addEventListener("click", function(){
     var childB = document.getElementById("Child Mode");
     var adminB = document .getElementById("Admin Mode");
     var passB = document.getElementById("sign_in");
 
-    
     childB.style.display = "none";
     adminB.style.display = "block";
     passB.style.display = "none";
