0618beaba71c5293c2ca7ecaee0848d64d27fe1e
Amanda La <anla@buffalo.edu>
2020-09-27 21:31:26 -0400

    #34 defined functions that affect GUI display based on desired mode, fixed bugs, password mode able to admin mode

--- axel_extension/popup.js
+++ axel_extension/popup.js
@@ -4,6 +4,47 @@
 
 //Detailed below is what allows buttons to react to being clicked
 
+// function clearLog(id){
+//   document.getElementById(id);
+// }
+
+//    Call to switch display to Admin Mode
+function displayAdminMode(adminDisplay, childDisplay, signInDisplay, signUpDisplay){
+  adminDisplay.style.display = "block";
+  childDisplay.style.display = "none";
+  signInDisplay.style.display = "none";
+  signUpDisplay.style.display = "none";
+  return;
+}
+
+//    Call to switch display to Child Mode
+function displayChildMode(adminDisplay, childDisplay, signInDisplay, signUpDisplay){
+  adminDisplay.style.display = "none";
+  childDisplay.style.display = "block";
+  signInDisplay.style.display = "none";
+  signUpDisplay.style.display = "none";
+  return;
+}
+
+//    Call to switch display to Create Password Mode (new admin)
+function displaySignUpMode(adminDisplay, childDisplay, signInDisplay, signUpDisplay){
+  adminDisplay.style.display = "none";
+  childDisplay.style.display = "none";
+  signInDisplay.style.display = "none";
+  signUpDisplay.style.display = "block";
+  return;
+}
+
+//    *******broken******
+// //    Call to switch display to Sign In Password Mode (returning admin)
+// function displaySignIpMode(adminDisplay, childDisplay, signInDisplay, signUpDisplay){
+//   adminDisplay.style.display = "none";
+//   childDisplay.style.display = "none";
+//   signInDisplay.style.display = "block";
+//   signUpDisplay.style.display = "none";
+//   return;
+// }
+
 //Implements an event listener to the pop up itself
 document.addEventListener('DOMContentLoaded', function(){
 	
@@ -13,9 +54,14 @@ document.addEventListener('DOMContentLoaded', function(){
     var childB = document.getElementById("Admin Mode");
     var sign_in = document.getElementById("sign_in");
     var create_pass = document.getElementById("create_pass");
+    var sign_up_form = document.getElementById("new_pass");
+    var sign_in_form = document.getElementById("entered_pass");
+    const log = document.getElementById('log');
     childB.style.display = "none";
     sign_in.style.display = "none";
     create_pass.style.display = "none";
+    sign_up_form.reset();
+    sign_in_form.reset();
   
   /************************************************************************/
   //		The below section Allows for Switching to the Child Mode Tab.
@@ -23,16 +69,22 @@ document.addEventListener('DOMContentLoaded', function(){
   //		with the removal of unwanted buttons.	
   //    sign_in and create_pass are added within this event listener in the event that
   //    a child/admin clicks the "Child Mode" button while within 
-  //    the create/enter password phase
+  //    the create/enter password phase. sign_up_form and sign_in_form get reset
+  //    so that characters input into those respective text fields do not stay 
+  //    inside the text field if Child Mode is clicked.
   /************************************************************************/
   var child = document.getElementById("default");
   child.addEventListener("click",function(){
     var adminB = document.getElementById("Admin Mode");
     adminB.style.display = "none";
     var childB = document.getElementById("Child Mode");
-    childB.style.display = "block";
-    sign_in.style.display = "none";
-    create_pass.style.display = "none";
+    // childB.style.display = "block";
+    // sign_in.style.display = "none";
+    // create_pass.style.display = "none";
+    displayChildMode(adminB, childB, sign_in, create_pass);
+    log.textContent = "";
+    sign_up_form.reset();
+    sign_in_form.reset();
   });
   
   /********************************************************************** */
@@ -54,8 +106,7 @@ document.addEventListener('DOMContentLoaded', function(){
     console.log('Value currently is '+ result.first_time);
     admin.setAttribute('value', result.first_time);
   });
-
-
+ 
   /*    admin.addEventListener()
     *    "element.target. value" grabs the value we stored in admin  
     *    and store the value in the variable "firstTime".
@@ -80,66 +131,75 @@ document.addEventListener('DOMContentLoaded', function(){
     var adminB = document .getElementById("Admin Mode");
     var sign_in = document.getElementById("sign_in");
     var create_pass = document.getElementById("create_pass");
+   
 
-    if(firstTime == 'true'){
-      childB.style.display = "none";
-      adminB.style.display = "none";
-      create_pass.style.display = "block";
-      sign_in.style.display = "none";
-
-      chrome.storage.sync.set({first_time: 'false'}, function(){
-        console.log('Value is set to ' + false);
-      });
-      firstTime = 'false';
-      admin.setAttribute('value', 'false');
-
-      const form = document.getElementById('new_pass');
-      form.addEventListener("submit", function(event){
-      
-        // chrome.storage.sync.set({password: 'meowmeow'}, function(){
-        //   console.log('Password entered to storage');
-        // });
-        childB.style.display = "none";
-        adminB.style.display = "block";
-        create_pass.style.display = "none";
-        form.reset();
+    //  is this a first time admin
+    if(firstTime == 'true'){                //  first time admin
+      displaySignUpMode(adminB, childB, sign_in, create_pass);
+      //  if submit button is hit, store entered password in storage and move into Admin Mode
+      sign_up_form.addEventListener("submit", function(event){
+        //sets first_time admin to false
+        chrome.storage.sync.set({first_time: 'false'}, function(){
+          console.log('Value is set to ' + false);
+        });
+        firstTime = 'false';
+        admin.setAttribute('value', 'false');
+        displayAdminMode(adminB, childB, sign_in, create_pass);
+        sign_up_form.reset();
         event.preventDefault();
-
-      } );
-    }
-    else{
+        //  grabs typed desired password
+        /**broken entered password store */
+      //   var desired_pass = "moonpies";
+      // //  var desired_pass = document.getElementById('creatingPass').value;
+      //   //  stores password entered into chrome storage
+      //   chrome.storage.sync.set({password:desired_pass }, function(){
+      //   // chrome.storage.sync.set({password:'aew;kjf;adf' }, function(){
+      //     console.log('Password entered to storage');
+      //   });
+        
+      } );  //end event listener
+      
+    } //end if
+    else{                                 //returning admin
       childB.style.display = "none";
       adminB.style.display = "none";
       create_pass.style.display = "none";
       sign_in.style.display = "block";
-      const form = document.getElementById('entered_pass');
-      const log = document.getElementById('log');
-      let stored_pass = document.getElementById('sign_in');
-      form.addEventListener("submit", function(event){
-        
+      log.textContent = "";
+      // displaySignInMode(adminB, childB, sign_in, create_pass);
+
+      //sign in using initially established password
+      sign_in_form.addEventListener("submit", function(event){
         //  grabs password input in text field
         var checkPass = document.getElementById('unique').value;
-
-        //  grabs created password from storage and stores passwork as a value in "stored_pass"
-        /* chrome.storage.sync.get('password', function(result){
-        //   console.log('Password grabbed from storage');
-        //   stored_pass.setAttribute('value', result.password);
-         });*/
         
-        //  currently password check is hardcoded to check if entered password != "meowmeow"
+        /** broken password check*/
+        /*// var passwords_match = false;
+        // chrome.storage.sync.get(['password'], function(result){
+        //   console.log('Password grabbed from storage');
+        //   // stored_pass = result.password;
+        //   log.textContent = result.password;
+        //   // log.textContent = "tried grabbing, here we are";
+        //   if(checkPass == result.password){
+        //     passwords_match = true;
+        //     // log.textContent = result.password;
+        //   }
+        //  });
+        */
+        // if(!passwords_match){
         if(checkPass != "meowmeow"){
           log.textContent = "incorrect password, please try again";
+          sign_in_form.reset();
           event.preventDefault();
         }
         //  entered password == "meowmeow", we move into Admin Mode
-        else{
+        else{ 
           //clears password input
-          form.reset();
-
-          //enters admin mode
-          childB.style.display = "none";
-          adminB.style.display = "block";
-          sign_in.style.display = "none";
+          sign_in_form.reset();
+          //resets log (text that states "incorrect password, please try again")
+          log.textContent = "";
+          // //enters admin mode
+          displayAdminMode(adminB, childB,sign_in, create_pass);
           event.preventDefault();
         }
       } );
